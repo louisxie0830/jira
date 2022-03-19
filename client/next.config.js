03f003bfa82df9678375568b16ab564441348351
Louis <bfgh2678@gmail.com>
2022-03-19 11:37:21 +0800

    create client

---
+++ client/next.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  rewrites: () => [
+    { source: "/api/:path*", destination: "http://localhost/:path*" },
+  ],
+});
