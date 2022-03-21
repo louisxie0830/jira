887a076ad2f33a0c45ee3457ef6fecda496a5b8a
Louis <bfgh2678@gmail.com>
2022-03-21 23:01:31 +0800

    update message

--- client/lib/component/AboutTooltip/index.js
+++ client/lib/component/AboutTooltip/index.js
@@ -17,22 +17,22 @@ export const AboutTooltip = tooltipProps => (
         </FeedbackImageCont>
 
         <FeedbackParagraph>
-          This simplified Jira clone is built with React on the front-end and Node/TypeScript on the
+          This simplified Jira clone is built with React on the front-end and Java/Spring Boot on the
           back-end.
         </FeedbackParagraph>
 
         <FeedbackParagraph>
           {'Read more on my website or reach out via '}
-          <a href="mailto:ivor@codetree.co">
-            <strong>ivor@codetree.co</strong>
+          <a href="mailto:bfgh2678@gmail.com">
+            <strong>bfgh2678@gmail.com</strong>
           </a>
         </FeedbackParagraph>
 
-        <a href="https://getivor.com/" target="_blank" rel="noreferrer noopener">
+        <a href="javascript:void(0)" target="_blank" rel="noreferrer noopener">
           <Button variant="primary">Visit Website</Button>
         </a>
 
-        <a href="https://github.com/oldboyxx/jira_clone" target="_blank" rel="noreferrer noopener">
+        <a href="javascript:void(0)" target="_blank" rel="noreferrer noopener">
           <Button style={{ marginLeft: 10 }} icon="github">
             Github Repo
           </Button>
