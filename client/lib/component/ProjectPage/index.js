9424db63b21db75e6da77dde84684fbee411c736
Louis <bfgh2678@gmail.com>
2022-03-23 13:01:14 +0800

    dev

---
+++ client/lib/component/ProjectPage/index.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import { sizes } from '../../styles/globalStyles';
+import { NavbarLeft } from '../NavbarLeft';
+import { Modal } from '../Modal'
+import { createQueryParamModalHelpers } from '../../utils/queryParamModal';
+
+const paddingLeft = sizes.appNavBarLeftWidth + sizes.secondarySideBarWidth + 40;
+
+export const ProjectPage = styled.div`
+  padding: 25px 32px 50px ${paddingLeft}px;
+
+  @media (max-width: 1100px) {
+    padding: 25px 20px 50px ${paddingLeft - 20}px;
+  }
+  @media (max-width: 999px) {
+    padding-left: ${paddingLeft - 20 - sizes.secondarySideBarWidth}px;
+  }
+`;
+
+const issueSearchModalHelpers = createQueryParamModalHelpers('issue-search');
+const issueCreateModalHelpers = createQueryParamModalHelpers('issue-create');
+
+const Project = () => {
+  return (
+    <ProjectPage>
+      <NavbarLeft
+        issueSearchModalOpen={issueSearchModalHelpers.open}
+        issueCreateModalOpen={issueCreateModalHelpers.open}
+      />
+      {/* {issueSearchModalHelpers.isOpen() && (
+        <Modal
+          isOpen
+          testid="modal:issue-search"
+          variant="aside"
+          width={600}
+          onClose={issueSearchModalHelpers.close}
+        />
+      )}
+
+      {issueCreateModalHelpers.isOpen() && (
+        <Modal
+          isOpen
+          testid="modal:issue-create"
+          width={800}
+          withCloseIcon={false}
+          onClose={issueCreateModalHelpers.close}
+        />
+      )} */}
+    </ProjectPage>
+  );
+};
+
+export default Project;
--- client/pages/project.js
+++ client/pages/project.js
@@ -1,32 +1,13 @@
-import styled from 'styled-components';
-import { sizes } from '../lib/styles/globalStyles';
-import { NavbarLeft } from '../lib/component/NavbarLeft';
-import { createQueryParamModalHelpers } from '../lib/utils/queryParamModal';
+import dynamic from 'next/dynamic'
 
-const paddingLeft = sizes.appNavBarLeftWidth + sizes.secondarySideBarWidth + 40;
-
-export const ProjectPage = styled.div`
-  padding: 25px 32px 50px ${paddingLeft}px;
-
-  @media (max-width: 1100px) {
-    padding: 25px 20px 50px ${paddingLeft - 20}px;
-  }
-  @media (max-width: 999px) {
-    padding-left: ${paddingLeft - 20 - sizes.secondarySideBarWidth}px;
-  }
-`;
+const ProjectPage = dynamic(
+  () => import('../lib/component/ProjectPage'),
+  { ssr: false }
+)
 
 const Project = () => {
-  const issueSearchModalHelpers = createQueryParamModalHelpers('issue-search');
-  const issueCreateModalHelpers = createQueryParamModalHelpers('issue-create');
-  
   return (
-    <ProjectPage>
-      <NavbarLeft
-        issueSearchModalOpen={issueSearchModalHelpers.open}
-        issueCreateModalOpen={issueCreateModalHelpers.open}
-      />
-    </ProjectPage>
+    <ProjectPage />
   );
 };
 
